74c47d5066b929bf7237b625446bf6090ab81690
Osvaldo Rojas <osvaldo.rojas@emergys.com>
2019-08-16 09:23:04 -0500

    cambios para iconos de estiba

--- app/src/main/java/com/example/simulador_pescado/vista/Estiba.java
+++ app/src/main/java/com/example/simulador_pescado/vista/Estiba.java
@@ -1,4 +1,23 @@
 package com.example.simulador_pescado.vista;
 
 public class Estiba {
+
+    private int posicion;
+    private Boolean disponible;
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public Boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(Boolean disponible) {
+        this.disponible = disponible;
+    }
 }
