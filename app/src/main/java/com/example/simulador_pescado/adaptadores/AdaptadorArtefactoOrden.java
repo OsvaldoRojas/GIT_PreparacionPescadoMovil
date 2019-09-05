986b8211f43386bf57cc0b32228082f490b1198f
Osvaldo Rojas <osvaldo.rojas@emergys.com>
2019-09-05 17:52:05 -0500

    cambios servicios

--- app/src/main/java/com/example/simulador_pescado/adaptadores/AdaptadorArtefactoOrden.java
+++ app/src/main/java/com/example/simulador_pescado/adaptadores/AdaptadorArtefactoOrden.java
@@ -1,4 +1,33 @@
 package com.example.simulador_pescado.adaptadores;
 
-public class AdaptadorArtefactoOrden {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.simulador_pescado.R;
+import com.example.simulador_pescado.vista.Artefacto;
+
+import java.util.List;
+
+public class AdaptadorArtefactoOrden extends ArrayAdapter<Artefacto> {
+
+    public AdaptadorArtefactoOrden(Context contexto, List<Artefacto> listaArtefacto){
+        super(contexto, 0, listaArtefacto);
+    }
+
+    @Override
+    public View getView(int posicion, View vista, ViewGroup padre) {
+        Artefacto artefacto = getItem(posicion);
+        if(vista == null){
+            vista = LayoutInflater.from( getContext() ).inflate(R.layout.item_artefacto_orden, padre, false);
+        }
+
+        TextView etiquetaDescripcion = vista.findViewById(R.id.etiquetaDescripcion);
+        etiquetaDescripcion.setText( artefacto.getDescripcion() );
+
+        return vista;
+    }
 }
